47ea5cabfd705dfb12b5e2858e14d154cd2ffa89
Kim-Su-B <hbsubin1@naver.com>
2024-01-05 14:12:00 +0900

    메인화면

---
+++ T_main/android/app/src/main/java/com/example/fastwaiting/notification_list.kt
@@ -0,0 +1,11 @@
+package com.example.fastwaiting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class notification_list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification_list)
+    }
+}
