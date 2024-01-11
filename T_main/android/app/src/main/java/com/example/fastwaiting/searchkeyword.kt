d1644b85d4bd69901767422c46f8e7580f3ac2af
(Jaemin) <(kjm9049123@gmail.com)>
2024-01-12 03:02:56 +0900

    회원가입 기워드 검색

---
+++ T_main/android/app/src/main/java/com/example/fastwaiting/searchkeyword.kt
@@ -0,0 +1,11 @@
+package com.example.fastwaiting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class searchkeyword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_searchkeyword)
+    }
+}
