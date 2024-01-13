cbe783ef41246d652d975f94e572c4081e8d60b9
ppark__jjong <101304928+Parkjonghyeok2@users.noreply.github.com>
2024-01-13 10:29:48 +0900

    .

---
+++ Park/Test_jwt/jwt/src/main/java/org/example/jwt/filter/MyFilter1.java
@@ -0,0 +1,20 @@
+package org.example.jwt.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyFilter1 implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        String headerAuth = req.getHeader("Authorization");
+        System.out.println(headerAuth);
+        chain.doFilter(req, res);
+    }
+}
