52ab1f393091a60c6c580e3e15a402a59c21b16e
DESKTOP-QKA1HE3\ojhye <ojhyeon930@naver.com>
2024-01-26 04:47:08 +0900

    01.26 페이팅 사장님 센터 - 회원가입 페이지 추가

--- Oh/fastwaitingadmin/src/redux/reducers.js
+++ Oh/fastwaitingadmin/src/redux/reducers.js
@@ -1,18 +1,22 @@
 const initialState = {
-    user: null,
-  };
-  
-  const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'LOGIN_USER':
-        return {
-          ...state,
-          user: action.payload,
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default rootReducer;
-  
+  user: null,
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'LOGIN_USER':
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case 'REGISTER_USER':
+      return {
+        ...state,
+        user: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
