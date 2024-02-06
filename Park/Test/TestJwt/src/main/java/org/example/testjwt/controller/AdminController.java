456a77f364b38b1b5b9d05d800521cf4137b5215
ppark__jjong <101304928+Parkjonghyeok2@users.noreply.github.com>
2024-02-06 15:06:34 +0900

    로직 구현 - 박종혁

---
+++ Park/Test/TestJwt/src/main/java/org/example/testjwt/controller/AdminController.java
@@ -0,0 +1,12 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+public class AdminController {
+    @GetMapping("/admin")
+    public String adminP() {
+
+        return "admin Controller";
+    }
+}
